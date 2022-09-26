1858a9a281e73c21a5fe074d707c56d52edf3014
Shpadin <shpadin@gmail.com>
2022-09-26 14:27:55 +0500

    посыпал сахаром

--- Program.cs
+++ Program.cs
@@ -1,4 +1,24 @@
 ﻿
+Console.WriteLine("Введите номер задачи 19 / 21 / 23");
+int task = int.Parse(Console.ReadLine());
+if (task == 19) 
+{
+    task1();
+}
+else if (task == 21)
+{
+    Task2();
+}
+else if (task == 23)
+{
+    Task3();
+}
+else
+{
+    Console.WriteLine("введон не верный номер задания");
+}
+void task1()
+{
 
 // Задача 19
 
@@ -8,31 +28,31 @@
 
 // 12821 -> да
 
-// 23432 -> да
+// 23432 -> даConsole.WriteLine("Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
+Console.WriteLine("Введите пятизначное число: ");
 
-// Console.WriteLine("Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.");
-// Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int amount = number.Length;
 
-// string number = Console.ReadLine();
-// int amount = number.Length;
+if (amount > 5)
+{
+    Console.WriteLine($"число: {number} - не является пятизначным");
+}
+else
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"число {number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"число {number} - НЕ палиндром");
+    }
+}
+}
 
-// if (amount > 5)
-// {
-//     Console.WriteLine($"число: {number} - не является пятизначным");
-// }
-// else
-// {
-//     if (number[0] == number[4] && number[1] == number[3])
-//     {
-//         Console.WriteLine($"число {number} - Палиндром");
-//     }
-//     else
-//     {
-//         Console.WriteLine($"число {number} - НЕ палиндром");
-//     }
-// }
-
-
+void Task2()
+{
 // Задача 21
 
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
@@ -42,24 +62,26 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 
-// Console.WriteLine("Введите координаты точки А по X:");
-// int x1 = int.Parse(Console.ReadLine());
-// Console.WriteLine("Введите координаты точки А по Y:");
-// int y1 = int.Parse(Console.ReadLine());
-// Console.WriteLine("Введите координаты точки А по Z:");
-// int z1 = int.Parse(Console.ReadLine());
-// Console.WriteLine("Введите координаты точки В по X:");
-// int x2 = int.Parse(Console.ReadLine());
-// Console.WriteLine("Введите координаты точки В по Y:");
-// int y2 = int.Parse(Console.ReadLine());
-// Console.WriteLine("Введите координаты точки В по Z:");
-// int z2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки А по X:");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки А по Y:");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки А по Z:");
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки В по X:");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки В по Y:");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки В по Z:");
+int z2 = int.Parse(Console.ReadLine());
 
-// double resalt =  Math.Sqrt(Math.Pow((x1-x2),2) + Math.Pow((y1-y2),2)+ Math.Pow((z1+z2),2));
+double resalt =  Math.Sqrt(Math.Pow((x1-x2),2) + Math.Pow((y1-y2),2)+ Math.Pow((z1+z2),2));
 
-//  Console.WriteLine($"рачтоние между точками {Math.Round(resalt,2)}");
+ Console.WriteLine($"рачтоние между точками {Math.Round(resalt,2)}");
 
-
+}
+void Task3()
+{
 // Задача 23
 
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
@@ -80,7 +102,7 @@ double resalt = 0;
         i++;
  }
 
-
+}
 // Необязательная к выполнению задача (не будет влиять на итоговую оценку ДЗ)
 // Дополнительная задача (задача со звёздочкой): Напишите программу, которая на вход принимает радиус круга и находит его площадь округленную до целого числа, необходимо вывести максимальную цифру в полученном округлённом значении площади круга.
 
